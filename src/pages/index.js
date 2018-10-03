bf35347b708af7855280e2ee253ccc7a6a42f120
Manvel Saroyan <saroyanm@gmail.com>
2018-10-03 13:33:33 +0200

    Added twitter feed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import { Timeline } from 'react-twitter-widgets'
+
 import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Hello there!</h1>
+    <p>I'm looking forward to participate in <a href="https://hackathon.freecodecamp.org/">freeCodeCamp JAMstack Hackathon</a></p>
+    <p><a href="‎https://twitter.com/jamstackconf">@jamstackconf</a> twitter feed below👇</p>
+    <Timeline
+    dataSource={{
+      sourceType: 'profile',
+      screenName: 'jamstackconf'
+    }}
+    options={{
+      username: 'jamstackconf',
+      height: '600',
+      width: '300'
+    }}
+  />
   </Layout>
 )
 
